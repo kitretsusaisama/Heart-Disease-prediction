455369306b06684f53e54b5e5e015f8dac4d9d41
Kiteretsu Daihyakka <98268964+kitretsusaisama@users.noreply.github.com>
2024-05-29 06:41:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ heart_model = joblib.load("models/heart_disease_model.sav")
 # sidebar
 with st.sidebar:
     selected = option_menu('Multiple Disease Prediction', [
-         'Heart disease Prediction',
-        'Disease Prediction',
+        'Heart disease Prediction',
         'Diabetes Prediction',
         'Parkison Prediction',
         'Liver prediction',
@@ -36,42 +35,6 @@ with st.sidebar:
     ],
         icons=['','activity', 'heart', 'person','person','person','person','bar-chart-fill'],
         default_index=0)
-
-
-
-
-# multiple disease prediction
-if selected == 'Disease Prediction': 
-    # Create disease class and load ML model
-    disease_model = DiseaseModel()
-    disease_model.load_xgboost('model/xgboost_model.json')
-
-    # Title
-    st.write('# Disease Prediction using Machine Learning')
-
-    symptoms = st.multiselect('What are your symptoms?', options=disease_model.all_symptoms)
-
-    X = prepare_symptoms_array(symptoms)
-
-    # Trigger XGBoost model
-    if st.button('Predict'): 
-        # Run the model with the python script
-        
-        prediction, prob = disease_model.predict(X)
-        st.write(f'## Disease: {prediction} with {prob*100:.2f}% probability')
-
-
-        tab1, tab2= st.tabs(["Description", "Precautions"])
-
-        with tab1:
-            st.write(disease_model.describe_predicted_disease())
-
-        with tab2:
-            precautions = disease_model.predicted_disease_precautions()
-            for i in range(4):
-                st.write(f'{i+1}. {precautions[i]}')
-
-
 
 
 # Diabetes prediction page
